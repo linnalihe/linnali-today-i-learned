6d8a151114c0cfeacbde816d30cb4bc17cb4eb7c
Linna Li <linnali@Linnas-Air.lan>
2024-02-06 10:37:46 -0500

    Quartz sync: Feb 6, 2024, 10:37 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Linna Li",
+    pageTitle: "Linna L",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
